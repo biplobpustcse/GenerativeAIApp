a5b11b6f945cde2434930ba78573348b63623ed3
biplobpustcse <biplobpustcse@gmail.com>
2025-04-24 16:05:35 +0600

    1

--- GenerativeAIApp.API/Controllers/AuthController.cs
+++ GenerativeAIApp.API/Controllers/AuthController.cs
@@ -50,7 +50,6 @@ namespace GenerativeAIApp.API.Controllers
             var newToken = _tokenService.GenerateRefreshToken(token);
             return newToken is null ? BadRequest("Invalid token") : Ok(new { token = newToken });
         }
-
         #endregion
     }
 }
